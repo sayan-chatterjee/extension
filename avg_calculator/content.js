1316c2ca3538f831e937b4ed891d9f7d1870ad81
Sayan Chatterjee (Consultant) <Sayan.Chatterjee@in.ril.com>
2018-11-01 18:59:36 +0530

    input box disabled, calculation done on page onload, also implemented for the NA logic

--- avg_calculator/content.js
+++ avg_calculator/content.js
@@ -1,10 +1,18 @@
 var EMPTY_STRING = "";
+/*
 var calculateButton = '<div class=" pmFooterButtonSpacing">'+
 '<span id="_calculate_span" class="buttonset sfDialogBoxButtonWrapper"><span>'+
 '<button style="background-color: azure;" class="globalRoundedCornersXSmall globalSecondaryButton buttonDefault" id="_calculate_button" type="button" accesskey="2">Calculate</button>'+
 '</span></span></div>';
 var footerButtons = $("#footer_buttons_rev").html();
-$("#footer_buttons_rev").html(calculateButton + footerButtons);
+*/
+var empWghtdAvgTextInput = $("#sect_7 div[class='HRContentCell'] input");
+var raWghtdAvgTextInput = $("#sect_8 div[class='HRContentCell'] input");
+//$("#footer_buttons_rev").html(calculateButton + footerButtons);
+
+//Disabling the input text boxes
+empWghtdAvgTextInput.prop('disabled', true);
+raWghtdAvgTextInput.prop('disabled', true);
 
 function getQuarterId() {
     var quarter = $("div[class='sfStepsChartNameContainer'] div[class='sfStepsChartStepName sfStepsChartStepNamePosition']")
@@ -28,23 +36,23 @@ function createKraList(propLeftTables, propRightTables) {
     var goalWeightageFound = false;
     for(var i = 0; i < propLeftTables.length; i++) {
         var dataFound = false;
-        var kra = {'goalWeightage':0, 'raInput':0, 'empInput':0};
+        var kra = {'goalWeightage':NaN, 'raInput':NaN, 'empInput':NaN};
         var leftLabel = $(propLeftTables[i]).find("td[class='lab']").html().toLowerCase();
         var leftValue = $(propLeftTables[i]).find("td[class='val']").html();
         //console.log('lLabel :', leftLabel);
         //console.log('lvalue :', leftValue);
         if("goal weightage" === leftLabel) {
-            leftValue = (null == leftValue || EMPTY_STRING === leftValue)? '0.00%': leftValue;
-            kra.goalWeightage = parseFloat(leftValue.replace('%',''));
+            leftValue = (null == leftValue || EMPTY_STRING === leftValue) ? NaN : leftValue;
+            kra.goalWeightage = Number.isNaN(leftValue) ? NaN : parseFloat(leftValue.replace('%',''));
             dataFound = true;
             goalWeightageFound = true;
         }
         if(-1 != leftLabel.indexOf(ra_achievement_label)) {
             if(goalWeightageFound){
-                leftValue = (null == leftValue || EMPTY_STRING === leftValue)? '0.00%': leftValue;
+                leftValue = (null == leftValue || EMPTY_STRING === leftValue) ? NaN : leftValue;
                 kra = kraList.pop();
                 //console.log('last item', kra);
-                kra.raInput = parseFloat(leftValue.replace('%',''));
+                kra.raInput = Number.isNaN(leftValue) ? NaN : parseFloat(leftValue.replace('%',''));
                 dataFound = true;
                 goalWeightageFound = false;
             }
@@ -56,15 +64,15 @@ function createKraList(propLeftTables, propRightTables) {
     }
     //console.log('kraList :', kraList);
     var count = 0;
-    for(var i = 0; i < propRightTables.length; i++) {     
+    for(var i = 0; i < propRightTables.length; i++) { 
         var rightLabel = $(propRightTables[i]).find("td[class='lab']").html().toLowerCase();
         var rightValue = $(propRightTables[i]).find("td[class='val']").html();
         //console.log('rLabel :', rightLabel);
         //console.log('rValue :', rightValue);
         if(-1 != rightLabel.indexOf(emp_achievement_label)) {
-            rightValue = (null == rightValue || EMPTY_STRING === rightValue)? '0.00%': rightValue;
+            rightValue = (null == rightValue || EMPTY_STRING === rightValue) ? NaN : rightValue;
             //console.log('kra :',kraList[(i+4)/4-1]);
-            kraList[count++].empInput = parseFloat(rightValue.replace('%',''));
+            kraList[count++].empInput = Number.isNaN(rightValue) ? NaN : parseFloat(rightValue.replace('%',''));
         }
     }
     //console.log('kraList :',kraList);
@@ -88,11 +96,12 @@ function calculateAchievement(kraList){
         'empActual' : (empActualAchievement/100).toFixed(2),
         'raActual' : (raActualAchievement/100).toFixed(2)
     }
-    //console.log('result :',result);
+    //console.log('Result :',result);
     return result;
 }
 
-$("#_calculate_button").click(function(){
+function calculate(){
+    //console.log('called calculate()');
     var quarterId = getQuarterId();
     var quarter = $(quarterId);
     //console.log('quarterId :',quarterId);
@@ -123,7 +132,10 @@ $("#_calculate_button").click(function(){
     var kraList = createKraList(propLeftTablesList, propRightTablesList);
     var result = calculateAchievement(kraList);
 
-    $('#wf_sect_7_e_0').val(result.empActual+' %');
-    $('#wf_sect_8_e_ele_10').val(result.raActual+' %');
-    
+    empWghtdAvgTextInput.val(isNaN(result.empActual)?'NA':(result.empActual+' %'));
+    raWghtdAvgTextInput.val(isNaN(result.raActual)?'NA':(result.raActual+' %'));   
+}
+
+$(document).ready(function(){
+    calculate();
 });
