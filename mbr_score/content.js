a4c9936f02939b91ade736fdb14a164751ad4f0b
Sayan Chatterjee (Consultant) <Sayan.Chatterjee@ril.com>
2019-07-11 17:33:15 +0530

    first draft for calculation

--- mbr_score/content.js
+++ mbr_score/content.js
@@ -5,7 +5,7 @@ var SCORE_KEY = "Score";
 var STATUS_KEY = "Status";
 
 var STATUS_ACTIVE = "Active";
-var STATUS_DROP = "Drop";
+var STATUS_DROP = "Drop (Specify reason in 'Achievement so far' comment box)";
 var STATUS_DROP_RTNG = "Drop but considered for rating";
 
 function getData() {   
@@ -44,10 +44,22 @@ function getData() {
 }
 
 function calculateScore(dataArray){
+    var adjustment = 0;
+    var mbr_score = 0;
     dataArray.forEach(element => {
         console.log(element);
         //Calculation logic to be written
+        adjustment += (STATUS_DROP_RTNG === element.STATUS_KEY ? element.WEIGHTAGE_KEY : 0);
+        mbr_score += (STATUS_ACTIVE === element.STATUS_KEY) ? (element.RATING_KEY / element.WEIGHTAGE_KEY) : 0;
     });
+    //console.log("adjustment : ", adjustment);
+    var annual_mbr_score = mbr_score * (100 - adjustment);
+    //console.log("annual_mbr_score : ", annual_mbr_score);
+    var annual_mbr_element = $("div h2[title='MBR Annual Score']");
+    var mbr_score_element = annual_mbr_element.parent().siblings("div").find("div[class='HRContentCell'] input");
+    //console.log(mbr_score_element.html());
+    mbr_score_element.val(annual_mbr_score.toFixed(2));
+    mbr_score_element.prop("readonly", true);
 };
 
 $(document).ready(function(){
